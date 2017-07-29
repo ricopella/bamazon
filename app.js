e551df776c134d7b41dce57eb7456140a37acfc8
Narin Sundarabhaya <nrs710@gmail.com>
2017-07-28 22:25:30 -0700

    challenge 1 completed

--- app.js
+++ app.js
@@ -14,21 +14,77 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+// global storage of current catalog
+let product_catalog = [];
+let product_catalog_names = [];
+
 function showInventory() {
-
-    connection.query('SELECT * FROM products', function(error, results, fields) {
+    connection.query('SELECT * FROM products', function(error, results) {
         if (error) throw error;
-        // console.log(`| Product ID | Product Name | Department | Price | Quantity |`);
-        console.log(`-------------------------------------------------------------`.yellow); // console.log(columnify(`|  ${results[i].item_id}  |  ${results[i].product_name}  |  ${results[i].department_name} |  ${results[i].price}  |  ${results[i].stock_quantity}  |`));
+        console.log(`------------------------------------------------------------------------------------`.yellow); // console.log(columnify(`|  ${results[i].item_id}  |  ${results[i].product_name}  |  ${results[i].department_name} |  ${results[i].price}  |  ${results[i].stock_quantity}  |`));
         console.log(columnify(results, { columns: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'] }))
+        console.log(`------------------------------------------------------------------------------------`.yellow);
 
-        console.log(`-------------------------------------------------------------`.yellow);
+        // removes RowDataPacket 
+        var newResults = JSON.parse(JSON.stringify(results));
 
+        // store data in new arrays for catalog selection
+        newResults.forEach(function(element) {
+            product_catalog_names.push(element.product_name);
+        }, this);
+        newResults.forEach(function(element) {
+            product_catalog.push(element);
+        }, this);
+
+        purchase();
     });
 
+
+}
+
+function purchase() {
+    console.log("");
+    inquirer.prompt([{
+            name: "purchase_id",
+            message: "Which product would you like to purchase?",
+            type: "list",
+            choices: product_catalog_names
+        },
+        {
+            name: "purchase_amount",
+            message: "How many units would you like to purchase?",
+            type: "input"
+        }
+    ]).then(function(answers) {
+        // refrences id of chosen product
+        let chosen_id = product_catalog_names.indexOf(answers.purchase_id) + 1;
+
+        // stores product selected by user to edit db
+        let chosen_product = product_catalog[chosen_id - 1];
+
+        // validate if product is in stock
+        if (chosen_product.stock_quantity < answers.purchase_amount) {
+            console.log(`\n - - - - -  - - - - \n`.red);
+            console.log('Insufficient quantity!'.red);
+            console.log(`\n - - - - -  - - - - \n`.red);
+
+            purchase();
+        } else {
+            // store current stock amount
+            let current_quantity = chosen_product.stock_quantity - answers.purchase_amount;
+            // update db of stock quantity
+            connection.query(`UPDATE products SET stock_quantity = ${current_quantity} WHERE item_id = ${chosen_id}`, function(error, results) {
+                console.log(`\n - - - - -  - - - - \n`.green);
+                console.log(`Your total will be: $${chosen_product.price * answers.purchase_amount}`);
+                console.log(`\n - - - - -  - - - - \n`.green);
+
+                showInventory();
+            })
+        }
+    });
 }
 
 showInventory();
 
 
-connection.end();
+// connection.end();
